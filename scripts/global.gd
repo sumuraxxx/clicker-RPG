edca6d6fbfea4b9bb1a819426a29a199cc6e4b03
sumuraxxx <143743357+sumuraxxx@users.noreply.github.com>
2023-11-02 09:36:42 +0300

    Global map 1.1
    исправил баги

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,3 +3,7 @@ extends Node
 const HEALTH: int = 25
 var player_damage: int = 10
 var player_health = HEALTH
+
+func health_recovary():
+	player_health = HEALTH
+	
--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -3,4 +3,5 @@ extends Node2D
 @onready var label = $CanvasLayer/Label
 
 func _ready():
+	Player.health_recovary()
 	label.text = "wwave: " + str(Counter.count_wave)
