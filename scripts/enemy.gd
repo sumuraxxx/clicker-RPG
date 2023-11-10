c4e4c04ed24a6410e70ec70f025d69a5bb1e938a
sumuraxxx <143743357+sumuraxxx@users.noreply.github.com>
2023-11-10 19:24:48 +0300

    Revert "refactoring"
    This reverts commit 48c04ee8648451d6e14ff1a742e38b48f58f184d.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,12 +5,9 @@ extends Area2D
 @export var damage_speed: int = 1
 
 var player_attack = Player.player_damage
-var cooldown = damage_speed
-var can_attack = false
-
+var timer = damage_speed
 @onready var scene = get_tree().current_scene.get_scene_file_path()
 @onready var enemy_health_text = $HealtCount
-
 
 
 func _ready():
@@ -19,24 +16,20 @@ func _ready():
 	print_debug(Player.player_health)
 
 func _physics_process(delta):
-	timer(delta)
-	if can_attack:
-		Player.damage_take(damage_enemy)
-		
+	if timer <= 0:
+		attack()
+		timer = damage_speed
+	else:
+		timer -= delta	
+	
 	if Player.player_health <= 0:
 		Player.player_health = 0		 
 		get_tree().change_scene_to_file("res://scenes/game_end_scene.tscn")		
 	print_debug(Player.player_health)
 	
-func timer(delta):
-	if cooldown <= 0:
-		can_attack = true
-		cooldown = damage_speed
-	else:
-		can_attack = false
-		cooldown -= delta	
-	
-	
+func attack():
+	Player.player_health -= damage_enemy
+
 
 func _on_button_pressed():
 	health_enemy -= player_attack
